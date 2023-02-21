e1177d88e4cd32b4c6975fa64858ca7fe47349f1
Santiago Benito <bmsanti@hotmail.com>
2023-02-21 19:38:16 +0100

    Añadido inicio y header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BorrarTareaComponent } from './componentes/borrar-tarea/borrar-tarea.component';
+import { FinalizarTareaComponent } from './componentes/finalizar-tarea/finalizar-tarea.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { TareasComponent } from './componentes/tareas/tareas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'tareas', component: TareasComponent },
+  { path: 'finalizarTarea/:id', component: FinalizarTareaComponent },
+  { path: 'borrarTareas', component: BorrarTareaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
