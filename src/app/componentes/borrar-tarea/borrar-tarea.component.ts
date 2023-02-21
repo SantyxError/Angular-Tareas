9df82a154a2af72e2b57192c40d1830797a4e72e
Santiago Benito <bmsanti@hotmail.com>
2023-02-21 20:20:23 +0100

    Añadido "Borrar Tarea"

--- src/app/componentes/borrar-tarea/borrar-tarea.component.ts
+++ src/app/componentes/borrar-tarea/borrar-tarea.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Tarea } from 'src/app/modelos/tarea';
+import { TareasService } from 'src/app/Servicios/tareas.service';
 
 @Component({
   selector: 'app-borrar-tarea',
   templateUrl: './borrar-tarea.component.html',
-  styleUrls: ['./borrar-tarea.component.css']
+  styleUrls: ['./borrar-tarea.component.css'],
 })
 export class BorrarTareaComponent {
+  constructor(private servicioTareas: TareasService) {}
 
+  tareas: Array<Tarea> = this.servicioTareas.getTareas();
+
+  ngOnInit() {
+    this.tareas;
+  }
+
+  borrarTarea(id: string) {
+    this.servicioTareas.deleteTarea(id);
+  }
 }
--- src/app/componentes/tarea/tarea.component.ts
+++ src/app/componentes/tarea/tarea.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Tarea } from 'src/app/modelos/tarea';
+import { TareasService } from 'src/app/Servicios/tareas.service';
 
 @Component({
   selector: 'app-tarea',
   templateUrl: './tarea.component.html',
-  styleUrls: ['./tarea.component.css']
+  styleUrls: ['./tarea.component.css'],
 })
 export class TareaComponent {
+  @Input() tarea!: Tarea;
+  @Output() borrarTarea = new EventEmitter();
+  ngOnInit() {}
 
+  eliminarTarea(id: string) {
+    this.borrarTarea.emit(id);
+  }
 }
